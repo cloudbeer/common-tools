ec30ed46756c22c4cd092c07508c9295e4df4556
Cloudbeer <cloudbeer@gmail.com>
2015-08-31 09:55:33 +0800

    add http tools

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var stringTools = require("./lib/string-tools");
 var validateTools = require("./lib/validate-tools");
+var httpClient = require("./lib/http-client");
 
 module.exports.stringTools = stringTools;
 
 module.exports.validateTools = validateTools;
+
+module.exports.httpClient = httpClient;
